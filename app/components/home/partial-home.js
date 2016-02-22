087a7230a200e26a3c97b7f869075268a4f351ee
Morten Rummelhoff <morten@mhr.dk>
2016-02-22 22:29:43 +0100

    Updated to angular 1.4.x.... bower.json needs to be aligned

--- app/components/home/partial-home.js
+++ app/components/home/partial-home.js
@@ -7,27 +7,15 @@ partialHomeController.controller('PartialHomeController', ['$scope', 'DateExtrac
 
     $scope.mhrname = "Hello PartialHome - This is the Controller speaking";
 
-    var date = new Date();
-    var day = date.getDate();
-    var month = date.getMonth() + 1;
-    var year = date.getFullYear();
-
-    if (month < 10) month = "0" + month;
-    if (day < 10) day = "0" + day;
-
-    var today = year + "-" + month + "-" + day;
-
-    $scope.d1 = today;
-    $scope.d2 = today;
+    $scope.d1 = new Date();
+    $scope.d2 = new Date();
 
     $scope.totalDays = 0;
     $scope.calc = function() {
         console.log("Calculate called, Date1: " + $scope.d1 +  ", Date2: " + $scope.d2);
 
-        var date1Array = $scope.d1.split("-");
-        var date2Array = $scope.d2.split("-");
-
-        DateExtractor.getDayExtract(date1Array[0], date1Array[1], date1Array[2], date2Array[0], date2Array[1], date2Array[2], function(response) {
+        DateExtractor.getDayExtract($scope.d1.getFullYear(), $scope.d1.getMonth(), $scope.d1.getDate(),
+            $scope.d2.getFullYear(), $scope.d2.getMonth(), $scope.d2.getDate(), function(response) {
             $scope.totalDays = response;
         });
 
